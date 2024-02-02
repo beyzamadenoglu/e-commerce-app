0eb02e38eb4e11686c89479f2b72d7806d480164
beyzakafein <beyza.madenoglu@kafein.com.tr>
2024-02-02 14:48:21 +0300

    project structure create

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+import path from 'path';
+
+const __filename = new URL(import.meta.url).pathname;
+const __dirname = path.dirname(__filename);
+
+const nextConfig = {
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
+};
 
 export default nextConfig;
